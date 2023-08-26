d8ce36c35b08b9ed12dffa3065c20c04e2a633a0
karthick <itkarthi02@gmail.com>
2023-08-26 17:34:55 +0530

    add metadata info

--- main.rb
+++ main.rb
@@ -1,9 +1,57 @@
 require 'open-uri'
+require 'nokogiri'
+require 'yaml'
 
-ARGV.each do |url|
-  begin
-    File.write(url.strip.delete_prefix('https://')&.concat('.html'), URI.open(url).read)
-  rescue Errno::ENOENT => e
-    puts "Error while opening this url : ", url
+ScrapperMetadata = Struct.new(:site, :num_links, :images, :last_fetch)
+
+class WebScraper
+
+  @@metadata_hash = YAML.load_file('metadata.yml') || {}
+
+  def self.scrap_url(url, url_name)
+    html_data = URI.open(url).read
+    #parse webpage
+    html_parser = Nokogiri::HTML.fragment(html_data)
+    images_count = html_parser.css('img').count
+    links_count = html_parser.css('a').count
+    curr_metadata = ScrapperMetadata.new(url, links_count, images_count, Time.now.utc.strftime("%a %b %d %Y %H:%M UTC"))
+    return html_data, curr_metadata.to_h
   end
-end 
+
+  def self.strip_url(url)
+    url.strip.delete_prefix('https://')&.concat('.html')
+  end
+
+  def self.process_urls(urls)
+    urls.each do |url|
+      begin
+        url_name = url.strip.delete_prefix('https://')&.concat('.html')
+        html_data, curr_metadata =  scrap_url(url, url_name)
+        #save file and metadata
+        File.write(url_name, html_data)
+        @@metadata_hash[url_name] = curr_metadata
+        File.write('metadata.yml', @@metadata_hash.to_yaml)
+      rescue Errno::ENOENT => e
+        puts "Error while opening this url : ", url
+      end
+    end
+  end
+
+  def self.read_metadata(urls)
+    urls.each do |url|
+      url_name = strip_url(url)
+      puts "\n"
+      @@metadata_hash[url_name]&.keys&.each { |key| puts "#{key}: #{@@metadata_hash[url_name][key]} \n" } 
+    end
+  end
+end
+
+if(ARGV.length)
+  if(ARGV[0] === '--metadata')
+    puts 'show metadata'
+    WebScraper.read_metadata(ARGV.drop(1))
+  else
+    WebScraper.process_urls(ARGV)
+  end
+end
+
